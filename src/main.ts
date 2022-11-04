6e9cf5b918c8e909adf6597a0e88053fe3e3a7bc
onikarubi <printuserselect@gmail.com>
2022-11-04 14:49:50 +0900

    create dom classes and functions

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import './styles/style.scss';
 
-// /* カラーパレット要素の取得 */
+interface HtmlElement {
+  targetName: string;
+  getElements(): HTMLCollectionOf<Element>;
+  elementsChangeEvent(elements: HTMLCollectionOf<Element>): void;
+}
+
+// /* カラーパレット、svg要素の変更 */
 
 abstract class InputElement {
-  public targetElement: HTMLInputElement;
+  protected targetElement: HTMLInputElement;
   protected type: string;
 
   constructor(public elemId: string) {
@@ -11,96 +17,103 @@ abstract class InputElement {
     this.targetElement = document.getElementById(this.elemId) as HTMLInputElement;
   }
 
-  public getInputValue(): string { return this.targetElement.value; }
+  get getInputValue(): string { return this.targetElement.value; }
   abstract inputChangeEvent(svg: HTMLElement): void;
+  abstract displayProperty<T, U>(type: T, elemId: U);
 }
 
 class InputColor extends InputElement {
-  private attributeName: string = 'fill';
-
+  private attributeName: string;
   constructor(elemId: string) {
     super(elemId);
     this.type = 'color';
+    this.attributeName = 'fill';
   }
 
   public inputChangeEvent(svg: HTMLElement): void {
-    this.targetElement.addEventListener('change', () => {
+    this.targetElement.addEventListener('input', () => {
       svg.setAttribute(this.attributeName, this.targetElement.value)
     });
   }
-}
 
-class SvgColor {
-  private fill: string;
-  private rgba: string;
-  private arrayHex: Array<string>;
-  private hexToRgba: string;
-
-  constructor(fill: string = '', rgba: string = '') {
-    this.fill = fill;
-    this.rgba = rgba;
-    this.arrayHex = this.splitArrayHex(this.fill);
-    this.hexToRgba = this.changeHexToRgba(this.arrayHex);
+  public displayProperty<T, U>(type: T, elemId: U) {
+    return {
+      inputType: type,
+      targetId: elemId
+    }
   }
 
-  public getFillHex(): string {
-    if (this.fill != '') {
-      return this.fill;
+  public get typeValue(): string {
+    if (this.type) {
+      return this.type;
+    } else {
+      throw new Error('値が空です。')
     }
-
-    throw Error('値が空です。');
   }
 
-  private changeRgbaToHex(rgba: string) {
-    const rgbaArray: Array<string> = rgba.split(' ');
-
-  }
-
-  private changeHexToRgba(hexArray: Array<string>): string {
-    let substring: string = '';
-    for (let i = 0; i < hexArray.length; i++) {
-      substring += hexArray[i];
-
-      if (!(i == hexArray.length - 1)) {
-        substring += ' ';
-      }
+  public get elemIdValue(): string {
+    if (this.elemId) {
+      return this.elemId;
+    } else {
+      throw new Error('値が空です。')
     }
-
-    return substring;
-  }
-
-  private splitArrayHex(hex: string): Array<string> {
-    const hexArray: Array<string> = []
-    const hexValue = hex.replace('#', '');
-
-    if (!(hexValue.length == 6)) {
-      throw Error();
-    }
-
-    for (let i = 0; i < hexValue.length; i += 2) {
-      hexArray.push(hexValue.substring(i, i + 2))
-    }
-    return hexArray;
   }
 }
 
-const svg = document.getElementById('svg');
+const svgBox: HTMLCollectionOf<Element> = document.getElementsByClassName('svg-box');
+const svgElements: HTMLCollectionOf<Element> = document.getElementsByClassName('svg');
+const colorElements: HTMLCollectionOf<Element> = document.getElementsByClassName('color-pallet');
 
-const colorPallet = document.getElementById('color-pallet') as HTMLInputElement;
-
-colorPallet.addEventListener('change', () => {
-  svg.setAttribute('fill', colorPallet.value);
-})
-
-
-
-
-
+const elementChangeEvent = (elements: HTMLCollectionOf<Element>, event: Function): void => {
+  for (const element of elements) {
+    event(element);
+  }
+}
+elementChangeEvent(svgElements, console.log)
 
 
+// let counter: number = 0;
+// console.log(getGenerateId(svgBox.length, 'color-pallet'))
 
 
 
+// const setAttributesId = (id: number): void => {
+//   svgElements[id].setAttribute('id', `svg-${id + 1}`);
+//   colorElements[id].setAttribute('id', `color-pallet-${id + 1}`);
+// }
+
+// const changeSvgColor = (id: number): void => {
+//   const svg: HTMLElement = document.getElementById(`svg-${id + 1}`);
+//   const inputElement: InputColor = new InputColor(`color-pallet-${id + 1}`);
+//   inputElement.inputChangeEvent(svg);
+// }
+
+// for (let i = 0; i < svgBox.length; i++) {
+//   let referencedId: number = i;
+//   setAttributesId(referencedId);
+//   changeSvgColor(referencedId);
+// }
+
+// const svgIdList: Array<string> = [];
+// const colorIdList: Array<string> = [];
+
+// for (let i = 0; i < svgElements.length; i++) {
+//   svgElements[i].setAttribute('id', `svg${i + 1}`);
+//   colorElements[i].setAttribute('id', `color-pallet-${i + 1}`);
+//   svgIdList.push(svgElements[i].getAttribute('id'))
+//   colorIdList.push(colorElements[i].getAttribute('id'))
+// }
+
+// const sampleInput = new InputColor('color-pallet-1')
+// const sampleInput2 = new InputColor('color-pallet-2')
+// const sampleInput3 = new InputColor('color-pallet-3')
+// const sampleSVG = document.getElementById('svg1')
+// const sampleSVG2 = document.getElementById('svg2')
+// const sampleSVG3 = document.getElementById('svg3')
+
+// sampleInput.inputChangeEvent(sampleSVG);
+// sampleInput2.inputChangeEvent(sampleSVG2);
+// sampleInput3.inputChangeEvent(sampleSVG3);
 
 
 
